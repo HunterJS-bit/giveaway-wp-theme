156426e87f18575316212e6c4c7473efd19bf7b7
Marko <markomiljkovic2@gmail.com>
2019-11-21 00:33:53 +0100

    updated pagination for posts

--- home.php
+++ home.php
@@ -7,32 +7,41 @@
                  <h2><a href="/" class="font-bold text-3xl text-gray-800">Blog</a></h2>       
             </nav>
             <?php 
-                $car_id = get_the_category()[0]->cat_ID;
-                $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-                $args = array( 
-                  'order'=>'DESC',
-                  'posts_per_page' => 6,
-                  'paged' => $paged
+                $paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
+ 
+                $args = array(
+                    'post_type' => 'post',
+                    'post_status'=>'publish',
+                    'posts_per_page' => 6,
+                    'paged' => $paged,
                 );
-                $scheduled = new WP_Query( $args );
+                $query = new WP_Query( $args );
 
                 global $wp_query;
             
-                if ( $scheduled->have_posts() ) : 
+                if ( $query->have_posts() ) : 
                 ?>
                  <div class="posts flex flex-wrap content-between mt-4">
-                  <?php while( $scheduled->have_posts() ) : $scheduled->the_post() ?>
+                  <?php while( $query->have_posts() ) : $query->the_post() ?>
                       <?php get_template_part( 'template-parts/content', 'card'); ?>
                     <!-- Display Post Here -->
                   <?php endwhile ?>
                   </div>
-                  <?php 
-                        echo "<div class='page-nav-container mt-10'>" . paginate_links(array(
-                          'total' => $scheduled->max_num_pages,
-                          'prev_text' => __('<<'),
-                          'next_text' => __('>>')
-                      )) . "</div>"; 
-                  ?>
+                  <div class="pagination text-center pt-16">
+            
+                    <?php
+                      $big = 999999999; // need an unlikely integer
+                     
+                      echo paginate_links( array(
+                          'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                          'format' => '?paged=%#%',
+                          'current' => max( 1, get_query_var('paged') ),
+                          'total' => $query->max_num_pages,
+                          'prev_text' => __('&#8592; Prethodna '),
+                          'next_text' => __('Sledeca &#8594;'),
+                      ));
+                    ?>
+                  </div>
                 <?php else : ?>
                   <!-- Content If No Posts -->
               <?php endif ?>
