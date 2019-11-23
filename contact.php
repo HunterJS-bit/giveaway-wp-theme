d4e91bb2a5af37d7ae27febf4d871f5ed502f3ec
Marko <markomiljkovic2@gmail.com>
2019-11-23 02:27:14 +0100

    started workingf on contact page

---
+++ contact.php
@@ -0,0 +1,93 @@
+<?php /* Template Name: Contact */ ?>
+
+<?php get_header() ?>
+
+<main class="container h-full flex-grow">
+    <h1 class="font-semibold uppercase text-2xl lg:text-3xl mt-12 md:mt-24 text-center pb-5">Kontakt<span class="mx-auto w-24 h-1 block mt-1 border-b-2 border-yellow-500"></span></h1>
+    <p class="w-full md:w-1/2 mx-auto text-center mb-2 mb-12">Interested in working with us? Whether it's a simple question or your next big idea, great products start with a conversation.</p>
+    <div class="flex flex-wrap mb-4">
+        <div class="w-full md:w-1/3 text-center p-4">
+            <h2 class="font-semibold uppercase text-1xl lg:text-2xl text-center pb-5">Posaljite nam Email<span class="mx-auto w-24 h-1 block mt-1 border-b-2 border-yellow-500"></span></h2>
+            <p>For project enquiries please use the form below otherwise please email us <a href="mailto:hello@arctica.io">hello@arctica.io</a></p>
+            <h2 class="font-semibold uppercase text-1xl lg:text-2xl text-center pb-5 mt-5">Pratite nas<span class="mx-auto w-24 h-1 block mt-1 border-b-2 border-yellow-500"></span></h2>
+            <ul class="flex justify-center">
+                <li class="opacity-50 hover:opacity-100 mr-2">
+                    <a style="color:#fff" href="https://github.com/arcticahq" target="_blank" rel="noopener noreferrer" aria-label="GitHub on GitHub">
+                        <svg style="display:inline-block;vertical-align:middle" width="20px" height="20px" viewBox="0 0 496 512">
+                            <path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
+                        </svg>
+                    </a>
+                </li>
+                <li class="opacity-50 hover:opacity-100 mr-2">
+                    <a style="color:#fff" href="https://twitter.com/arcticahq" target="_blank" rel="noopener noreferrer" aria-label="Twitter on Twitter">
+                        <svg style="display:inline-block;vertical-align:middle" width="20px" height="20px" viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
+                        </svg>
+                    </a>
+                </li>
+                <li class="opacity-50 hover:opacity-100 mr-2">
+                    <a style="color:#fff" href="https://www.linkedin.com/company/arcticahq" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn on LinkedIn">
+                        <svg style="display:inline-block;vertical-align:middle" width="20px" height="20px" viewBox="0 0 448 512">
+                            <path fill="currentColor" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path>
+                        </svg>
+                    </a>
+                </li>
+                <li class="opacity-50 hover:opacity-100 mr-2">
+                    <a style="color:#fff" href="https://angel.co/arctica" target="_blank" rel="noopener noreferrer" aria-label="Angel List on Angel List">
+                        <svg style="display:inline-block;vertical-align:middle" width="20px" height="20px" viewBox="0 0 448 512">
+                            <path fill="currentColor" d="M347.1 215.4c11.7-32.6 45.4-126.9 45.4-157.1 0-26.6-15.7-48.9-43.7-48.9-44.6 0-84.6 131.7-97.1 163.1C242 144 196.6 0 156.6 0c-31.1 0-45.7 22.9-45.7 51.7 0 35.3 34.2 126.8 46.6 162-6.3-2.3-13.1-4.3-20-4.3-23.4 0-48.3 29.1-48.3 52.6 0 8.9 4.9 21.4 8 29.7-36.9 10-51.1 34.6-51.1 71.7C46 435.6 114.4 512 210.6 512c118 0 191.4-88.6 191.4-202.9 0-43.1-6.9-82-54.9-93.7zM311.7 108c4-12.3 21.1-64.3 37.1-64.3 8.6 0 10.9 8.9 10.9 16 0 19.1-38.6 124.6-47.1 148l-34-6 33.1-93.7zM142.3 48.3c0-11.9 14.5-45.7 46.3 47.1l34.6 100.3c-15.6-1.3-27.7-3-35.4 1.4-10.9-28.8-45.5-119.7-45.5-148.8zM140 244c29.3 0 67.1 94.6 67.1 107.4 0 5.1-4.9 11.4-10.6 11.4-20.9 0-76.9-76.9-76.9-97.7.1-7.7 12.7-21.1 20.4-21.1zm184.3 186.3c-29.1 32-66.3 48.6-109.7 48.6-59.4 0-106.3-32.6-128.9-88.3-17.1-43.4 3.8-68.3 20.6-68.3 11.4 0 54.3 60.3 54.3 73.1 0 4.9-7.7 8.3-11.7 8.3-16.1 0-22.4-15.5-51.1-51.4-29.7 29.7 20.5 86.9 58.3 86.9 26.1 0 43.1-24.2 38-42 3.7 0 8.3.3 11.7-.6 1.1 27.1 9.1 59.4 41.7 61.7 0-.9 2-7.1 2-7.4 0-17.4-10.6-32.6-10.6-50.3 0-28.3 21.7-55.7 43.7-71.7 8-6 17.7-9.7 27.1-13.1 9.7-3.7 20-8 27.4-15.4-1.1-11.2-5.7-21.1-16.9-21.1-27.7 0-120.6 4-120.6-39.7 0-6.7.1-13.1 17.4-13.1 32.3 0 114.3 8 138.3 29.1 18.1 16.1 24.3 113.2-31 174.7zm-98.6-126c9.7 3.1 19.7 4 29.7 6-7.4 5.4-14 12-20.3 19.1-2.8-8.5-6.2-16.8-9.4-25.1z"></path>
+                        </svg>
+                    </a>
+                </li>
+                <li class="opacity-50 hover:opacity-100 mr-2">
+                    <a style="color:#fff" href="https://www.facebook.com/arcticahq" target="_blank" rel="noopener noreferrer" aria-label="Facebook on Facebook">
+                        <svg style="display:inline-block;vertical-align:middle" width="20px" height="20px" viewBox="0 0 448 512">
+                            <path fill="currentColor" d="M448 56.7v398.5c0 13.7-11.1 24.7-24.7 24.7H309.1V306.5h58.2l8.7-67.6h-67v-43.2c0-19.6 5.4-32.9 33.5-32.9h35.8v-60.5c-6.2-.8-27.4-2.7-52.2-2.7-51.6 0-87 31.5-87 89.4v49.9h-58.4v67.6h58.4V480H24.7C11.1 480 0 468.9 0 455.3V56.7C0 43.1 11.1 32 24.7 32h398.5c13.7 0 24.8 11.1 24.8 24.7z"></path>
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="w-full md:w-1/3 text-center p-4">
+            <h2 class="font-semibold uppercase text-1xl lg:text-2xl text-center pb-5">Find Us<span class="mx-auto w-24 h-1 block mt-1 border-b-2 border-yellow-500"></span></h2>
+            <p>We're based in Shoredich, the closest tube station is Old Street.</p>
+            <p class="mt-6">Arctica,
+                <br> 86-90 Paul Street,
+                <br>Shoreditch
+                <br>London
+                <br>EC2A 4NE</p>
+        </div>
+    </div>
+    <p class="text-center text-xs">Arctica Holdings Ltd is registered in England and Wales, company number 08024499 and VAT registration number GB 134 6271 25.</p>
+    <div class="mt-10 mb-10">
+        <div class="relative bg-white rounded-lg p-6 md:p-8 text-black shadow-lg">
+            <h2 class="font-semibold uppercase text-1xl lg:text-2xl text-center pb-5">Project Enquiry<span class="mx-auto w-24 h-1 block mt-1 border-b-2 border-yellow-500"></span></h2>
+            <p class="w-full md:w-1/2 mx-auto text-center mb-2">Please include as much information as possible about what you'd like to build, as well as budgets, timelines and your location.</p>
+            <form>
+                <div class="flex flex-wrap mt-10">
+                    <div class="w-full md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-black text-xs font-bold" for="name">Name
+                            <input type="text" aria-required="true" aria-invalid="false" value="" name="name" class="appearance-none mt-2 mb-6 block w-full bg-gray-100 text-gray-900 border rounded-lg py-3 px-3 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500" required="" id="name">
+                        </label>
+                        <label class="block uppercase tracking-wide text-black text-xs font-bold" for="email">Email
+                            <input type="text" aria-required="true" aria-invalid="false" value="" name="email" class="appearance-none mt-2 mb-6 block w-full bg-gray-100 text-gray-900 border rounded-lg py-3 px-3 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500" required="" id="email">
+                        </label>
+                        <label class="block uppercase tracking-wide text-black text-xs font-bold" for="company">Company<span class="text-gray-600"> (optional)</span>
+                            <input type="text" aria-required="false" aria-invalid="false" value="" name="company" class="appearance-none mt-2 mb-6 block w-full bg-gray-100 text-gray-900 border rounded-lg py-3 px-3 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500" id="company">
+                        </label>
+                    </div>
+                    <div class="w-full md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-black text-xs font-bold" for="message">What can we help you build?
+                            <textarea aria-required="true" aria-invalid="false" class="appearance-none h-20 mt-2 block w-full bg-gray-100 text-gray-800 border rounded-lg py-3 px-3 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500" style="min-height: 223px; height: 176px;" name="message" id="message" required="" maxlength="3000"></textarea>
+                        </label>
+                    </div>
+                </div>
+                <div class="flex justify-end mt-5 px-3">
+                    <button type="submit" formnovalidate="" class="focus:outline-none inline-block text-black text-xs py-3 px-4 rounded-full font-semibold uppercase cursor-pointer bg-indigo-600 rise text-white bg-white relative">
+                        <canvas style="border-radius:inherit;height:100%;left:0;position:absolute;top:0;width:100%" height="0" width="0"></canvas>Send message</button>
+                </div>
+            </form>
+        </div><span style="width:96%" class="h-6 bg-white opacity-50 mx-auto rounded z-20 -mt-3 block"></span><span style="width:92%" class="h-6 bg-white opacity-50 mx-auto rounded z-20 -mt-4 block"></span></div>
+</main>
+
+<?php get_footer() ?>
