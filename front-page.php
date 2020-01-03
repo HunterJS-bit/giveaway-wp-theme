56690affe3e00f03da87b9d5c2a1042ec3c45dc3
Marko <markomiljkovic2@gmail.com>
2020-01-03 15:15:28 +0100

    adding animation to main header elements title, text button & image

--- front-page.php
+++ front-page.php
@@ -9,7 +9,7 @@
       <div class="w-full md:w-1/2 h-full text-center">
           <div class="relative">
               <div>
-                  <div class="mx-auto md:w-full mx-auto text-center flex align-center items-center justify-center">
+                  <div class="mx-auto md:w-full mx-auto text-center flex align-center items-center justify-center animate-pop-bottom">
                     <img
                       src="<?php bloginfo('template_url'); ?>/images/nagrada_main.svg"
                       alt="Nagradna Igra"
@@ -20,13 +20,14 @@
       </div>
       <div class="left_block w-full md:w-1/2 text-center md:text-left py-2 md:py-16">
       <div class="md:w-full">
-        <h1 class="py-4 md:py-8 pb-8 text-grey-darkest text-4xl md:text-5xl font-bold">
+        <h1 class="py-4 md:py-8 pb-8 text-grey-darkest text-4xl md:text-5xl font-bold animate-pop-in header-title">
          Učestvujte u nagradnim igrama i osvojite vredne nagrade</h1>
       </div> 
-          <h2 class="text-grey-darker text-base md:text-2xl mx-auto leading-normal md:pr-4">
+          <h2 class="text-grey-darker text-base md:text-2xl mx-auto leading-normal md:pr-4 animate-pop-in header-subtitle">
           Pratite sve nagradne igre i konkurse na jednom mestu
           </h2> 
-          <a class="mx-auto rounded-lg mt-10 inline-block px-16 py-4 text-white text-lg bg-green-500 font-semibold shadow-xl cursor-pointer">
+          <a class="mx-auto rounded-lg mt-10 inline-block px-16 py-4 text-white text-lg bg-green-500 font-semibold shadow-xl cursor-pointer animate-pop-in header-button"
+            href="/blog">
           Priključite se </a>
   </div>   
 </div>
@@ -68,21 +69,21 @@
 	  </div><!-- #primary -->
   </section>
 
-    <section class="container lf-padding mx-auto flex flex-col py-8 pt-4 ">    
+    <section class="container lf-padding mx-auto flex flex-col py-8 pt-4">    
       <h2 class="text-4xl font-bold text-center text-gray-800 mb-8">
         O Nama
       </h2>
       <div class="md:flex justify-around text-center">
         <div class="w-full max-w-xs px-10 mb-10 ">
             <div class="mx-auto text-center">
-              <img src="<?php bloginfo('template_url'); ?>/images/nagradne-igre.org_osvoji.svg" alt="Nagrada Osvoji" class="mx-auto css-v7v99c">
+              <img src="<?php bloginfo('template_url'); ?>/images/nagradne-igre.org_osvoji.svg" alt="Nagrada Osvoji" class="mx-auto">
             </div>
             <h3 class="text-3xl text-gray-800 font-bold mb-3">Vidi i osvoji</h3>
             <p class="text-center">Otkrijte široku ponudu nagradnih igara, konkursa i online nagradnih igara</p>
         </div>
         <div class="w-full max-w-xs px-10 mb-10">
             <div class="mx-auto text-center">
-              <img src="<?php bloginfo('template_url'); ?>/images/nagradne-igre.org_prati.svg" alt="Nagrada PC" class="mx-auto css-v7v99c">
+              <img src="<?php bloginfo('template_url'); ?>/images/nagradne-igre.org_prati.svg" alt="Nagrada PC" class="mx-auto">
             </div>
             <h3 class="text-3xl text-gray-800 font-bold mb-3">Prati objave</h3>
             <p class="text-center">Objavljujemo i delimo svakog dana nagradne igre sa celog Balkana sa vama</p>
