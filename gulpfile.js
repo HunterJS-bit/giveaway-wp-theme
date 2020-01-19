a6bd9cf0bad755bc8d70db44f12b209c11d44181
Marko <markomiljkovic2@gmail.com>
2020-01-19 16:08:59 +0100

    feat: added purge css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
-const { watch } = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const purgecssWordpress = require('purgecss-with-wordpress');
+const Purgecss = require('purgecss').default;
+const fs = require('fs');
 
-gulp.task('style', function() {
+gulp.task('style', function () {
     var tailwindcss = require('tailwindcss');
-
+    // 511kb
     return gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss([
@@ -15,6 +17,16 @@ gulp.task('style', function() {
         .pipe(gulp.dest('./'));
 });
 
-// watch('sass/style.scss', () => {
-//     // gulp.start('style');
-// })
+
+const purgecss = new Purgecss({
+    content: ['**/*.php'],
+    css: ['**/*.css'],
+    whitelist: purgecssWordpress.whitelist,
+    whitelistPatterns: purgecssWordpress.whitelistPatterns
+});
+
+gulp.task('purge', async () => {
+
+    const result = await purgecss.purge();
+    fs.writeFileSync('production.css', result[0].css);
+});
